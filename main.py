45dd915ff4dc2893d0cec6e70835a91a5ff990ae
bobreddy2009 <bobreddy2009@yahoo.com>
2020-05-11 03:23:42 +0000

    i Finished

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+#My homework for the first day of class
 print (10 + 10)
 print (10 * 10)
 print (10 - 10)
 print (10/10)
  
 
-print ("45 * 45 = " + str(45 * 45))
+print ("45 * 45 = " + str(45 * 45))  
+print ("Avaneesh")
+
